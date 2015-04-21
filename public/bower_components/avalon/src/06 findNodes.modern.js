49af527a3a8a16bf296a6814d92239852370d4a2
IndexXuan <pengruihaha@163.com>
2015-04-22 01:00:02 +0800

    second commit in ubuntu to remove useless dir 20150422

--- public/bower_components/avalon/src/06 findNodes.modern.js
+++
@@ -1,6 +0,0 @@
-var ravalon = /(\w+)\[(avalonctrl)="(\S+)"\]/
-var findNodes = function(str) {
-    //pc safari v5.1: typeof DOC.querySelectorAll(str) === 'function'
-    //https://gist.github.com/DavidBruant/1016007
-    return DOC.querySelectorAll(str)
-} 
